95c4868918e38c4917f92d844290d39427b4f745
mxtowen <99655682@qq.com>
2017-10-31 23:09:09 +0800

    set C++ complite

--- first_cpp.cpp
+++ first_cpp.cpp
@@ -1,9 +1,33 @@
 
 #include <iostream>
+#include <vector>
 
-void main ()
+using namespace std;
+
+int main ()
 {
+    int x = 1;
 
-    getchar ();
+    [&](void)
+    {
+        x = 2;
+    };
+
+    while (x)
+    {
+        cout << "hello world!" << endl;
+        vector<int> age;
+        age.push_back(10);
+        age.push_back(12);
+        age.push_back(15);
+        age.push_back(30);
+        for (auto i : age)
+        {
+            cout << i << endl;
+        }
+        cin >> x;
+    }
+    
+    //getchar ();
     return 0;
 }
